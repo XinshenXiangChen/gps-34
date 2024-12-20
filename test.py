3fb17008d537b3922ddfef527d2f522dca8c2786
XinshenXiangChen <xinshenxiangchen@gmail.com>
2024-12-20 12:52:36 +0100

    more docs

--- test.py
+++ test.py
@@ -2,18 +2,21 @@
 import unittest
 import transform
 
-"""Test class"""
-class TestStringMethods(unittest.TestCase):
 
+class TestStringMethods(unittest.TestCase):
+    """Test class"""
     def test_is_upper(self):
+        """Test upper"""
         sting = transform.to_upper_case("hello")
         self.assertEqual(sting, "HELLO")
 
     def test_is_lower(self):
+        """Test lower"""
         sting = transform.to_lower_case("HELLO")
         self.assertEqual(sting, "hello")
 
     def test_is_capitalize(self):
+        """Test capitalize"""
         sting = transform.to_capitalize("HELLO")
         self.assertEqual(sting, "Hello")
 
